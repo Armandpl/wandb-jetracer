e93c8f6fa90a898cc79e3e3bbe3546566f4bb554
sean <adpl33@gmail.com>
2021-04-30 16:52:48 +0200

    add todo to data collection step

--- 3_trt_optim.py
+++ 3_trt_optim.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     with wandb.init(project="wandb-jetracer", job_type="trt-optimization") as run:
         
         print("downloading non optimized model")
-        artifact = run.use_artifact('model:v10')
+        artifact = run.use_artifact('model:latest')
         artifact_dir = artifact.download()
 
 
         print("creating model architecture")
-        # fetching the model architecture from the producer urn
+        # fetching the model architecture from the producer run
         producer_run = artifact.logged_by()
         model = torchvision.models.__dict__[producer_run.config['architecture']](pretrained=False)
         
@@ -29,12 +29,13 @@ if __name__ == "__main__":
         model_trt = torch2trt(model, [data], fp16_mode=True)
 
         # evaluate model
+        # TODO
 
         # save the model for inference
-        # print("saving model and uploading it to wandb...")
+        print("saving model and uploading it to wandb...")
         torch.save(model_trt.state_dict(), 'trt-model.pth')
 
-        # trt_artifact = wandb.Artifact('trt-model', type='model')
-        # trt_artifact.add_file('trt-model.pth')
-        # run.log_artifact(trt_artifact)
+        trt_artifact = wandb.Artifact('trt-model', type='model')
+        trt_artifact.add_file('trt-model.pth')
+        run.log_artifact(trt_artifact)
 
