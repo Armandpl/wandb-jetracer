7f9651a857cbda26a983efc965c23db96a767ea7
Armand <adpl33@gmail.com>
2021-05-11 19:01:38 +0200

    move setup_logging in utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,13 @@
+import logging
+
+# TODO: check if I can import without having torch installed?
+import PIL.Image
 import torch
 import torchvision.transforms as transforms
-import torch.nn.functional as F
-import cv2
-import PIL.Image
-import numpy as np
 
 mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
 std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
+
 
 def preprocess(image):
     device = torch.device('cuda')
@@ -14,3 +15,15 @@ def preprocess(image):
     image = transforms.functional.to_tensor(image).to(device)
     image.sub_(mean[:, None, None]).div_(std[:, None, None])
     return image[None, ...]
+
+
+def setup_logging(config=None):
+    if config is None:
+        logging_level = logging.INFO
+    else:
+        logging_level = logging.DEBUG if config.debug else logging.INFO
+
+    logging.basicConfig(
+        format='%(levelname)s: %(message)s',
+        level=logging_level
+    )
