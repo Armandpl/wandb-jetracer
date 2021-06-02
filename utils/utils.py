972af9b7918270a07c59e2f59a368f12152993a9
Armand <adpl33@gmail.com>
2021-06-02 16:32:55 +0200

    extract utils function from notebook to repo

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,21 @@
 import logging
+import os
+
+
+def make_dirs(output_dataset):
+    out_dirs = [os.path.join(output_dataset, split)
+                for split in ["train", "val", "test"]]
+
+    [os.makedirs(curr_dir) for curr_dir in out_dirs]
+
+    return out_dirs
+
+
+def split_list_by_pct(data, pcts):
+    sizes = [int(pct*len(data)) for pct in pcts]
+
+    it = iter(data)
+    return [[next(it) for _ in range(size)] for size in sizes]
 
 
 def setup_logging(config=None):
