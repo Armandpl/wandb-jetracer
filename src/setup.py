c4558941a4bc1219b204ffa307e61dcc71ff4535
Armandpl <adpl33@gmail.com>
2021-06-22 16:55:26 +0200

    fix setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,11 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="wandb_jetracer",
     version="0.0.1",
     author="Armand du Parc Locmaria",
     author_email="adpl33@gmail.com",
+    packages=find_packages(),
     description="A package supporting the scripts in \
                 https://github.com/Armandpl/wandb-jetracer",
     url="https://github.com/Armandpl/wandb-jetracer",
